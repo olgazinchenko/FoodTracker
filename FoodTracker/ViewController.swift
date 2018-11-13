dc52ae5bcbc5be5e018690fd50d257851ba00d56
Olga Zinchenko <ozinchenko@olgas-macbook-pro.local>
2018-11-13 11:10:43 +0200

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Olga Zinchenko on 11/13/18.
+//  Copyright © 2018 Olga Zinchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
