42e699c94394c87b3350ca5d105bbfdedf222e92
Olga Zinchenko <ozinchenko.pm@gmail.com>
2019-11-22 00:01:54 +0200

    Added a photo library usage description

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -60,9 +60,16 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         // The info dictionary may contain multiple representations of the image. You want to use the original.
-        guard (info[UIImagePickerController.InfoKey.originalImage] as? UIImage) != nil else {
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            else {
             fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
         }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
     }
 }
 
