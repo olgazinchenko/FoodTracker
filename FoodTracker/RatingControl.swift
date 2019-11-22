b96bce08969a32ca36b387091b7bb2488d0906ae
Olga Zinchenko <ozinchenko.pm@gmail.com>
2019-11-22 02:19:33 +0200

    Created a subclass of UIStackView

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Olga Zinchenko on 11/22/19.
+//  Copyright © 2019 Olga Zinchenko. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
