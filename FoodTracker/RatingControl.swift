6d86517f3f5165ab3288fbe839b67c21cb009eed
Olga Zinchenko <ozinchenko.pm@gmail.com>
2019-11-23 00:18:42 +0200

    Added inspectable properties

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+ @IBDesignable class RatingControl: UIStackView {
+    
+    //MARK: Properties
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    private var ratingButtons = [UIButton] ()
+    var rating = 0
     
     //MARK: Initialization
     override init(frame: CGRect) {
@@ -29,20 +43,26 @@ class RatingControl: UIStackView {
     //MARK: Private Methods
     private func setupButtons() {
         
+        for _ in 0..<starCount {
+        
         // Create the button
         let button = UIButton()
         button.backgroundColor = UIColor.red
         
         // Add constraints
-        button.translatesAutoresizingMaskIntoConstraints = true
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = false
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
            
-         // Setup the button action
-          button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
         
         // Add the button to the stack
         addArrangedSubview(button)
+            
+        // Add the new button to the rating button array
+        ratingButtons.append(button)
+        }
     }
 
 }
