436e4e998275749cd952c057aa1409591687ab1d
Olga Zinchenko <ozinchenko.pm@gmail.com>
2019-11-25 22:55:36 +0200

    Set star images for the buttons

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -43,7 +43,7 @@ import UIKit
     //MARK: Private Methods
     private func setupButtons() {
         
-        // clear any existing buttons
+        // Clear any existing buttons
         for button in ratingButtons {
             removeArrangedSubview(button)
             button.removeFromSuperview()
@@ -51,16 +51,29 @@ import UIKit
         
         ratingButtons.removeAll()
         
+        // Load Button Images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        
+        
         for _ in 0..<starCount {
 
         // Create the button
         let button = UIButton()
-        button.backgroundColor = UIColor.red
+            
+        // Set the button images
+        button.setImage(emptyStar, for: .normal)
+        button.setImage(filledStar, for: .selected)
+        button.setImage(highlightedStar, for: .highlighted)
+        button.setImage(highlightedStar, for: [.highlighted, .selected])
         
         // Add constraints
         button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
-            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+        button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+        button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
            
         // Setup the button action
         button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
