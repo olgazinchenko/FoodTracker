354b17f37259959ece34f3c5b248e7a88541c456
Olga Zinchenko <ozinchenko.pm@gmail.com>
2019-12-04 18:50:34 +0200

    Created a subclass of UITableViewCell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Olga Zinchenko on 12/4/19.
+//  Copyright © 2019 Olga Zinchenko. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
